1a391b7f063cb60e5284a68ecea4e73de3df1555
kmooney <kmooney@gmail.com>
2013-01-05 12:15:26 -0600

    Added JSONField docs

--- reportengine/jsonfield.py
+++ reportengine/jsonfield.py
@@ -1,3 +1,8 @@
+"""
+Django JSON Field.  This extends Django Model Fields to store JSON as a field-type.
+"""
+#TODO - Move this to utils or another application.  This is tangential to reporting and useful for other things.
+
 from django.db import models
 from django.utils import simplejson
 from django.core.serializers.json import DjangoJSONEncoder
@@ -6,6 +11,12 @@ import logging
 
 class JSONFieldDescriptor(object):
     def __init__(self, field, datatype=dict):
+        """
+        Create a JSONFieldDescriptor
+
+        :param field:   The field to create the descriptor for.
+        :param datatype:  The datatype of the descriptor.
+        """
         self.field = field
         self.datatype = datatype
 
@@ -48,6 +59,16 @@ class JSONField(models.TextField):
                  encoder=DjangoJSONEncoder(), decoder=simplejson.JSONDecoder(),
                  datatype=dict,
                  **kwargs):
+        """
+        Create a new JSONField
+
+        :param verbose_name:   The verbose name of the field
+        :param name:    The short name of the field.
+        :param encoder:   The encoder used to turn native datatypes into JSON.
+        :param decoder:   The decoder used to turn JSON into native datatypes.
+        :param datatype:  The native datatype to store.
+        :param kwargs:    Other arguments to pass to parent constructor.
+        """
         blank = kwargs.pop('blank', True)
         models.TextField.__init__(self, verbose_name, name, blank=blank,
                                   **kwargs)
@@ -55,15 +76,29 @@ class JSONField(models.TextField):
         self.decoder = decoder
         self.datatype = datatype
 
+    #TODO - Is this used anywhere?  If not, let's remove it.
     def db_type(self, connection=None):
+        """
+        Returns the database type.  Overrides django.db.models.Field's db_type.
+
+        :param connection:  The database connection - defaults to none.
+        :return:  The database type.  Always returns the string 'text'.
+        """
         return "text"
 
+
     def contribute_to_class(self, cls, name):
+        """
+        Overrides django.db.models.Field's contribute to class to handle descriptors.
+
+        :param cls:  The class to contribute to.
+        :param name: The name.
+        """
         super(JSONField, self).contribute_to_class(cls, name)
         setattr(cls, self.name, self.descriptor_class(self, self.datatype))
     
     def pre_save(self, model_instance, add):
-        "Returns field's value just before saving."
+        "Returns field's value just before saving.  If a descriptor, get's that instead of value from object."
         descriptor = getattr(model_instance, self.attname)
         if isinstance(descriptor, self.datatype):
             return descriptor
@@ -76,12 +111,27 @@ class JSONField(models.TextField):
         return super(JSONField, self).get_db_prep_save(value, *args, **kwargs)
 
     def value_to_string(self, obj):
+        """
+        Turns the value to a JSON string.
+        :param obj: An object.
+        :return:  A string.
+        """
         return self.dumps(self.value_from_object(obj))
 
     def dumps(self, data):
+        """
+        Encodes data and dumps.
+        :param data: A value.
+        :return:  An encoded string.
+        """
         return self.encoder.encode(data)
 
     def loads(self, val):
+        """
+
+        :param val:  A JSON encoddd string.
+        :return: A dict with data from val
+        """
         try:
             val = self.decoder.decode(val)#, encoding=settings.DEFAULT_CHARSET)
 
@@ -97,7 +147,11 @@ class JSONField(models.TextField):
         return val
     
     def south_field_triple(self):
-        "Returns a suitable description of this field for South."
+        """
+        Returns a suitable description of this field for South."
+
+        :return: A tuple of field_class, args and kwargs from South's introspector.
+        """
         # We'll just introspect the _actual_ field.
         from south.modelsinspector import introspector
         field_class = "django.db.models.fields.TextField"
